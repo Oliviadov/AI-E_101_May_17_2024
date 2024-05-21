85c5712e4224ff48fe91915d7ce90af711587341
Femi Imelda Olivia Murielle Dovoedo <168828438+Oliviadov@users.noreply.github.com>
2024-05-21 09:50:09 -0400

    Variables.py

--- Variables.py
+++ Variables.py
@@ -9,7 +9,7 @@ print(x + y) # output: 11
 print(x - y) # -1
 
 # Multiplication *: Left operand multiply by right operand
-print(x * y)  output: 30
+print(x * y) # output: 30
 
 # Exponentiation ** : Left operand raised to the power of right
 x = 2
@@ -33,3 +33,92 @@ print(x % y) # output: 2 (x is not even)
 x=16
 y=4
 print(x % y) # output 0 (x is even)
+
+# Assignment operators. The assignment operators are used to assign values to variables. The following table lists all the arithmetic operators in Python:
+# += # Increases the value of a variable by adding another value to it.
+x = 5
+x += 5
+print(x) # output: 10
+
+# -= # Subtracts a value from a variable and assigns the result to the same variable
+x = 5
+x -= 2
+print(x) # output: 3
+
+# *= # Mulptiply a value from a variable and assigns the result to the same variable
+x = 2
+x *= 3
+print(x) # output: 6
+
+# /= # Divide a value from a variable and assigns the result to the same variable
+x = 6
+x /= 3
+print(x) # output: 2.0
+
+# //= # Floor Divide and assigns the result to the same variable
+x = 6
+x //= 5
+print(x) # output: 1
+
+# %= # Modulo and assigns the result to the same variable
+x = 11
+x %= 3
+print(x) # output: 2
+
+#Boolean Operations
+## Comparison operators. The comparison operators compare two operands and return a boolean either True or False. The following table lists comparison operators in Python.
+# >, return True if the left operand is higher than the right one.
+x = 5
+y = 6
+print(x > y) # output: False 
+
+# <, return True if the left operand is lower than right one
+x = 5
+y = 6
+print(x < y) # output: True
+
+# ==, return True if the operands are equal
+x = 5
+y = 6
+print(x == y) # output: False
+
+# !=, return True if the operands are not equal
+x = 5
+y = 6
+print(x != y) # output : True
+
+# >=, return True if the left operand is higher than or equal to the right one
+x = 5
+y = 6
+print(x >= y) # output: False
+
+# <=, return True if the left operand is lower than or equal to the right one
+x = 5
+y = 6
+print(x <= y) # output: True
+
+# Logical operators.The logical operators are used to combine two boolean expressions. The logical operations are generally applicable to all objects, and support truth tests, identity tests, and boolean operations.
+# and, return True if both are true
+x = 5
+y = 6
+print( x > 1 and y < 10) # output: True
+
+# or, return True if at least one is true
+x = 5
+y = 6
+print( x > 1 or y < 10) # output: True
+
+# not, Returns True if an expression evalutes to false and vice-versa
+x = 5
+print(not x > 1) # output: False
+
+# Identity operators
+# is, return True if both are true
+x = 5
+y = 6
+print(x is y) # output: False
+
+# is not, return True if at least one is true
+x = 5
+y = 6
+print(x is not y) # output: True
