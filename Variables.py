de7b55a5395c878a933b8e540676f4f1e8cdf1aa
Femi Imelda Olivia Murielle Dovoedo <168828438+Oliviadov@users.noreply.github.com>
2024-05-21 08:46:59 -0400

    Variables.py

---
+++ Variables.py
@@ -0,0 +1,35 @@
+# Arithmetic_operators
+# Python operators : Arithmétic operators Arithmetic operators perform the common mathematical operation on the numeric operands.
+# The arithmetic operators return the type of result depends on the type of operands, as below.
+# Addition +: Sum of two operands
+x, y = 5, 6
+print(x + y) # output: 11
+
+# Subtraction -: Left operand minus right operand
+print(x - y) # -1
+
+# Multiplication *: Left operand multiply by right operand
+print(x * y)  output: 30
+
+# Exponentiation ** : Left operand raised to the power of right
+x = 2
+y = 3
+print(2 ** 3) # output: 8
+
+# Division /
+x = 6
+y = 3
+print(x/ y) # output: 2.0
+
+# Floor division // # Sends the quotient rounded down to the lower integer
+x = 6
+y = 5
+print(x // y) # output: 1
+
+# Modulus: Reminder of a/b % to see if a number is even. if x%y == 0, x is even
+x = 11
+y = 3
+print(x % y) # output: 2 (x is not even)
+x=16
+y=4
+print(x % y) # output 0 (x is even)
